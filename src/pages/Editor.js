282e90fde8a22e1f6ca9a8f8f09c825301649f06
Rafael Goncalves <capitao.rafa@gmail.com>
2021-09-06 23:43:17 -0300

    Added ReactPrismEditor

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,3 +1,20 @@
+import ReactPrismEditor from "react-prism-editor";
+
 export default function FavoritesPage() {
-    return <div><h1>Editor</h1></div>
+
+
+    let code='le code \n is big \n as big \n as my';
+    return <div><h1>Editor</h1>
+        <ReactPrismEditor
+            language={'cpp'}
+            theme={'dark'}
+            code={code}
+            lineNumber={true}
+            readOnly={false}
+            clipboard={false}
+            changeCode={code => {
+                // this.code=code
+                // console.log(code)
+            }}
+        /></div>
 }
