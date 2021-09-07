a6d2266ff587f5554a9763e4aad341eb34cecea7
Rafael Goncalves <capitao.rafa@gmail.com>
2021-09-07 00:14:14 -0300

    Going crazy

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,10 +1,13 @@
 import ReactPrismEditor from "react-prism-editor";
+import Diagram from "../components/Diagram"
 
 export default function FavoritesPage() {
 
 
     let code='le code \n is big \n as big \n as my';
     return <div><h1>Editor</h1>
+
+        <Diagram />
         <ReactPrismEditor
             language={'cpp'}
             theme={'dark'}
