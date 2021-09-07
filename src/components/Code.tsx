b77048e8d6de2214972c84ac3a22fd84afc10df4
Rafael Goncalves <capitao.rafa@gmail.com>
2021-09-07 13:51:41 -0300

    Gave up on linenumbers

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Prism from "prismjs";
-import "prismjs/themes/prism-tomorrow.css";
-
+// import "prismjs/themes/prism-tomorrow.css";
+import "./prism.css";
 export default function Code(props: { code: string, language: string }) {
     useEffect(() => {
         Prism.highlightAll();
     }, []);
     return (
         <div className="Code">
-            <pre>
+            <pre className="line-numbers">
                 <code className={`language-${props.language}`}>{props.code}</code>
             </pre>
         </div>
