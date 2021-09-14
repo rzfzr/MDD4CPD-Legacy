6931449c289e9681bdeeb174a5403f30ea48824f
Rafael Goncalves <capitao.rafa@gmail.com>
2021-09-14 10:45:47 -0300

    Switching to png

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import masterDiagram from '../assets/master.svg'
+import masterDiagram from '../assets/master.png'
 export default function HomePage() {
     return <div>
         <h2>
             Welcome to MDD4IoT!
         </h2>
         <div>
-            <img src={masterDiagram} alt="Architecture Diagram" style={{ backgroundColor: "white" }} />
+            <img src={masterDiagram} alt="Architecture Diagram" style={{ backgroundColor: "white", maxHeight: '80vh', maxWidth: '100%' }} />
         </div>
 
     </div>
