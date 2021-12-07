1b0ff6dcc69e1f0c8faf8e5163932e01c3206223
Rafael Goncalves <capitao.rafa@gmail.com>
2021-12-07 18:14:01 -0300

    Cleanup editableNode

--- src/components/react-diagrams/custom-node/custom_nodes/editableNode/EditableNodeModel.ts
+++ src/components/react-diagrams/custom-node/custom_nodes/editableNode/EditableNodeModel.ts
@@ -1,16 +1,14 @@
 import { EditablePortModel } from "./EditablePortModel";
 import _ from "lodash";
-import {NodeModel, PortModelAlignment} from '@projectstorm/react-diagrams'
+import { NodeModel, PortModelAlignment } from '@projectstorm/react-diagrams'
 export class EditableNodeModel extends NodeModel {
   content: string;
   constructor(
     content: string = "content"
   ) {
-    super({type: "Editable"});
+    super({ type: "Editable" });
     this.content = content;
-    this.addPort(new EditablePortModel(PortModelAlignment.TOP));
     this.addPort(new EditablePortModel(PortModelAlignment.LEFT));
-    this.addPort(new EditablePortModel(PortModelAlignment.BOTTOM));
     this.addPort(new EditablePortModel(PortModelAlignment.RIGHT));
   }
 
