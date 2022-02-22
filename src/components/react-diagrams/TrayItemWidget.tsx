69b517eed4afef7368ced189c45249a67a90b571
Rafael Goncalves <capitao.rafa@gmail.com>
2022-02-22 11:03:38 -0300

    Reusing diagram

--- src/components/react-diagrams/TrayItemWidget.tsx
+++ src/components/react-diagrams/TrayItemWidget.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import ReactTooltip from 'react-tooltip';
+import NodeDiagram from '../../components/NodeDiagram'
 export interface TrayItemWidgetProps {
 	node: any;
 }
@@ -44,29 +45,7 @@ export class TrayItemWidget extends React.Component<TrayItemWidgetProps> {
 					<OpenInNewIcon style={{ fontSize: '1rem' }} />
 				</a>
 				<ReactTooltip id={'tip-' + this.props.node.name} type='light' place="bottom">
-					<div style={{ width: '300px', height: '300px', backgroundColor: 'white', border: 'solid', fontSize: '1.3em' }}>
-						<div style={{ width: '100%', borderBottom: 'solid 3px', fontSize: '2em' }}>
-							{this.props.node.name}
-						</div>
-						<div style={{ width: '100%', borderBottom: 'solid 2px' }}>
-							Library={this.props.node.extras.library}
-						</div>
-						{this.props.node.ins && this.props.node.ins.length > 0 &&
-							this.props.node.ins.map((port: any) => {
-								return <div key={port}>
-									<p>{port}</p>
-								</div>
-							})
-						}
-						{this.props.node.outs && this.props.node.outs.length > 0 &&
-							this.props.node.outs.map((port: any) => {
-								return <div key={port}>
-									<p>{port}</p>
-								</div>
-							})
-						}
-					</div>
-					{/* {JSON.stringify(this.props.node)} */}
+					<NodeDiagram node={this.props.node} />
 				</ReactTooltip>
 			</S.Tray>
 		);
