70ad352b1f3956502a7a13216bf6e59f42000418
Rafael Goncalves <capitao.rafa@gmail.com>
2022-03-16 12:27:05 -0300

    Cleanup

--- src/pages/Models.tsx
+++ src/pages/Models.tsx
@@ -2,18 +2,19 @@ import GoClass from "../components/GoClass";
 import paletteNodes from "../paletteNodes";
 
 export default function ModelsPage() {
+    const startDelta = 1000
+    const endDelta = 2000
+    const controllerDelta = 3000
 
-    const defaultNodes: any[] = [
+    let nodesStatic: any[] = [
         { key: -1, name: 'MicroController' },
         { key: -2, name: 'Arduino' },
         { key: -3, name: 'Component' },
     ]
-    const defaultLinks: any[] = [
+    let linksStatic: any[] = [
         { key: -2, from: -2, to: -1, relationship: "generalization" },
         { key: -3, from: -3, to: -1, relationship: "generalization" },
     ]
-    let nodesStatic: any[] = [...defaultNodes]
-    let linksStatic: any[] = [...defaultLinks]
     let nodesDynamic: any[] = []
     let linksDynamic: any[] = []
 
@@ -33,7 +34,6 @@ export default function ModelsPage() {
             key: index,
             name: node.name,
             methods: methods
-            // properties:
         }
 
 
@@ -42,23 +42,14 @@ export default function ModelsPage() {
 
         if (node.extras.type === 'controller') {
             linksStatic.push({ key: index, from: index, to: -2, relationship: "generalization" })
-            // linksDynamic.push({ key: index, from: index, to: -2, relationship: "generalization" })
         } else {
-
-            const sD = 1000
-            const eD = 2000
-            const cD = 3000
-
-
-
-
-            nodesDynamic.push({ key: index + cD, name: 'MicroController' })
-            linksDynamic.push({ key: index + cD, from: index + sD, to: index + cD, text: "relation", relationship: "generalization" })
-            nodesDynamic.push({ key: index + sD, category: "Start" })
-            linksDynamic.push({ key: index + sD, from: index + cD, to: index, text: "relation", relationship: "generalization" })
+            nodesDynamic.push({ key: index + controllerDelta, name: 'MicroController' })
+            linksDynamic.push({ key: index + controllerDelta, from: index + startDelta, to: index + controllerDelta, text: "relation", relationship: "generalization" })
+            nodesDynamic.push({ key: index + startDelta, category: "Start" })
+            linksDynamic.push({ key: index + startDelta, from: index + controllerDelta, to: index, text: "relation", relationship: "generalization" })
             nodesDynamic.push(parsedNode)
-            linksDynamic.push({ key: index + eD, from: index, to: index + eD, text: "relation", relationship: "generalization" })
-            nodesDynamic.push({ key: index + eD, category: "End" })
+            linksDynamic.push({ key: index + endDelta, from: index, to: index + endDelta, text: "relation", relationship: "generalization" })
+            nodesDynamic.push({ key: index + endDelta, category: "End" })
         }
 
     });
