8115a536bda5cb66eaa1c48658e1a04063b0e556
Rafael Goncalves <capitao.rafa@gmail.com>
2022-04-28 11:06:12 -0300

    Replaced manual with published doc

--- src/pages/Manual.tsx
+++ src/pages/Manual.tsx
@@ -1,9 +1,10 @@
+// https://docs.google.com/document/d/13n98EJGDVv8c7dxrdF9a7BM8-vhUTmCj2Kx3hMkEQRw/edit
+
 export default function ManualPage() {
-
-
-    return <>
-        <div>
-            <iframe src={'../assets/manual.pdf'} />
-        </div>
-    </>
+    return <iframe
+        title="manual"
+        src="https://docs.google.com/document/d/e/2PACX-1vR2vJKeMlDD3hxRPtSCpQQevXOIGGWNwUvJCE4KQDpdl7kq4O2DR0LwX2Xhc2WmvdC8U-mo_hzyPyca/pub?embedded=true"
+        style={{ height: '90vh', width: '98vw' }}
+    >
+    </iframe>
 }
